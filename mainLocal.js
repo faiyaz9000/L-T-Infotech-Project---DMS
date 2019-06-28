a741f0dc7c7f33f0cffe91907daec3d291766962
arfatS <47104221+arfatS@users.noreply.github.com>
2019-06-28 17:52:21 +0530

    Add files via upload
    local storage changes 28th June

---
+++ mainLocal.js
@@ -0,0 +1,46 @@
+function GetUserList()
+{
+   return JSON.parse(localStorage.UseStorage);
+}
+ function RegisterUser()
+{
+
+   debugger;
+
+   try {
+   var datavalue={
+      Id:Math.random(),
+      FirstName : document.getElementById('firstName').value,
+      LastName: document.getElementById('lastName').value,
+      Mobile: document.getElementById('mobile').value,  
+      Email : document.getElementById('email').value,
+      Password :document.getElementById('password').value,
+      DOB : document.getElementById('dob').value,
+      Gender : document.getElementById('gender').value
+   };
+
+   var UserList=[];
+
+   // For the first time it will store the key
+   if(!localStorage.UseStorage)
+   {
+      UserList=[];
+   }else
+   {
+      //if not first time then get the existing list and store it
+      UserList= JSON.parse(localStorage.UseStorage);
+   }
+
+   UserList.push(datavalue);
+
+   localStorage.UseStorage= JSON.stringify(UserList);
+
+   alert('Registration Successful');
+   location.href="/Login.html";
+   
+} catch(ex)
+{
+   alert(ex.message);
+}
+
+}
